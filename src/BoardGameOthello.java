e1373c7280ee2152ec45a6408f9bf6e914562163
Wonkyu-Lee <wkyulees@gmail.com>
2018-03-15 22:58:40 +0900

    Updated.

--- src/BoardGameOthello.java
+++ src/BoardGameOthello.java
@@ -2,18 +2,6 @@ import java.util.*;
 
 public class BoardGameOthello {
 
-    enum Stone {
-        EMPTY, BLACK, WHITE;
-
-        Stone otherSide() {
-            switch (this) {
-                case BLACK: return Stone.WHITE;
-                case WHITE: return Stone.BLACK;
-                default: throw new IllegalArgumentException();
-            }
-        }
-    }
-
     static class Position {
         public final int row;
         public final int col;
@@ -39,11 +27,23 @@ public class BoardGameOthello {
         }
     }
 
-    static class Game {
+    static class Othello {
         public enum State {
             WAITING_INPUT,
-            STOP_TO_PASS,
+            WAITING_PASS,
             DONE,
+        }
+
+        enum Stone {
+            EMPTY, BLACK, WHITE;
+
+            Stone otherSide() {
+                switch (this) {
+                    case BLACK: return Stone.WHITE;
+                    case WHITE: return Stone.BLACK;
+                    default: throw new IllegalArgumentException();
+                }
+            }
         }
 
         private final Stone[][] slots;
@@ -51,7 +51,7 @@ public class BoardGameOthello {
         private Stone currentTurn = Stone.BLACK;
         private State state = State.WAITING_INPUT;
 
-        public Game(int rows, int cols) {
+        public Othello(int rows, int cols) {
             slots = new Stone[rows][cols];
             for (int i = 0; i < rows; ++i) {
                 for (int j = 0; j < cols; ++j) {
@@ -78,21 +78,21 @@ public class BoardGameOthello {
             return Collections.unmodifiableCollection(candidates);
         }
 
-        public boolean update(Position position) {
+        public boolean placeStone(int r, int c) {
             if (state != State.WAITING_INPUT) {
                 return false;
             }
 
-            if (!candidates.contains(position)) {
+            if (!candidates.contains(new Position(r, c))) {
                 return false;
             }
 
-            flipStones(position);
+            flipStones(r, c);
             toggleTurn();
             updateCandidates();
 
             if (candidates.isEmpty()) {
-                state = State.STOP_TO_PASS;
+                state = State.WAITING_PASS;
                 return false;
             }
 
@@ -100,9 +100,10 @@ public class BoardGameOthello {
         }
 
         public void pass() {
-            if (state == State.STOP_TO_PASS) {
+            if (state == State.WAITING_PASS) {
                 toggleTurn();
                 updateCandidates();
+
                 if (candidates.isEmpty()) {
                     state = State.DONE;
                 } else {
@@ -135,16 +136,16 @@ public class BoardGameOthello {
             return counts;
         }
 
-        private void flipStones(Position position) {
-            slots[position.row][position.col] = whoseTurn();
+        private void flipStones(int r, int c) {
+            slots[r][c] = currentTurn;
 
             int[][] directions = { {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1} };
             for (int i = 0; i < 8; ++i) {
                 int dr = directions[i][0];
                 int dc = directions[i][1];
 
-                int nr = position.row + dr;
-                int nc = position.col + dc;
+                int nr = r + dr;
+                int nc = c + dc;
 
                 LinkedList<Position> stonesToFlip = new LinkedList<>();
                 while (valid(nr, nc) && slots[nr][nc] == currentTurn.otherSide()) {
@@ -199,7 +200,7 @@ public class BoardGameOthello {
         }
 
         public void display(boolean markCandidates) {
-            System.out.printf("Turn of %s:\n", whoseTurn().toString());
+            System.out.printf("Turn of %s:\n", currentTurn.toString());
             for (int j = 0; j < slots[0].length; ++j) {
                 if (j == 0)
                     System.out.print("  ");
@@ -216,7 +217,7 @@ public class BoardGameOthello {
                     char ch = '·';
 
                     if (markCandidates && candidates.contains(new Position(i, j))) {
-                        ch = 'C';
+                        ch = '?';
                     }
 
                     switch(slots[i][j]) {
@@ -246,8 +247,8 @@ public class BoardGameOthello {
             }
 
             Map<Stone, Integer> count = countSlots();
-            int blacks = count.get(Stone.BLACK);
-            int whites = count.get(Stone.WHITE);
+            int blacks = count.getOrDefault(Stone.BLACK, 0);
+            int whites = count.getOrDefault(Stone.WHITE, 0);
             if (blacks > whites) {
                 return Stone.BLACK;
             } else if (blacks < whites) {
@@ -259,30 +260,34 @@ public class BoardGameOthello {
     }
 
     public static void main(String[] args) {
-        Game game = new Game(5, 5);
         Scanner scanner = new Scanner(System.in);
+
+        Othello game = new Othello(10, 10);
 
         while (!game.done()) {
             game.display(true);
 
-            System.out.println("Put stone: ");
+            System.out.println("Enter row and col:");
             int r = scanner.nextInt();
             int c = scanner.nextInt();
 
-            if (!game.update(new Position(r, c))) {
+            if (!game.placeStone(r, c)) {
                 switch (game.state()) {
                     case WAITING_INPUT:
                         System.out.println("Invalid position!");
                         break;
-                    case STOP_TO_PASS:
-                        System.out.println("No place available, pass!");
+                    case WAITING_PASS:
+                        System.out.printf("No place available for %s, pass!\n", game.whoseTurn().toString());
                         game.pass();
+                        if (game.state() == Othello.State.DONE) {
+                            System.out.printf("No place available for %s, pass!\n", game.whoseTurn().toString());
+                        }
                         break;
                 }
             }
         }
 
-        Stone winner = game.whoWon();
+        Othello.Stone winner = game.whoWon();
         System.out.printf("%s won!\n", winner);
     }
 }
